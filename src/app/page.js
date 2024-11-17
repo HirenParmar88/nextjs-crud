c7cb3f34dfbd1d9acb2703d017339c693e28ae20
HirenParmar88 <hirenparmar5105@gmail.com>
2024-11-17 12:34:57 +0530

    users sign up page ui design

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import React from 'react';
 import Link from 'next/link';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from '@/components/navigationBar/navigationBar.jsx';
-
+import UsersSignUp from '@/components/user/userSignUp.jsx';
 export default function Dashboard() {
 
   return (
@@ -27,7 +27,7 @@ export default function Dashboard() {
         </li><br/><br/>
       </ul>
     </div>
-    
+    <UsersSignUp />
     </>
   );
 }
